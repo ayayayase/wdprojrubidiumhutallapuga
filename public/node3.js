85717b295c97e2140d50e2cd55eb70cdedf5da03
Glitch (glitch-hello-node) <none>
2024-12-27 14:37:21 +0000

    🏇🔦 Checkpoint ./public/style3.css:70032858/182 ./public/node3.js:70032858/866

--- public/node3.js
+++ public/node3.js
@@ -5,3 +5,11 @@ document.getElementById("btnback1").onclick = function () {
 document.getElementById("changename").onclick = function changename () {
   document.getElementById("name").innerHTML = window.prompt("Enter desired name");
 }
+
+document.getElementById("changedialogue").onclick = function changedialogue () {
+  // Get the current value of the #dialogueinput field
+  var dialogueinput = document.getElementById("dialogueinput").value;
+  
+  // Update the #dialogue element with the new input value
+  document.getElementById("dialogue").innerHTML = dialogueinput;
+}
