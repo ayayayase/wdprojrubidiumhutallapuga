a2450d404f83a76fab72c332632f43cccc8472a6
Glitch (glitch-hello-node) <none>
2024-12-27 14:14:12 +0000

    🌍🚌 Checkpoint ./public/style3.css:70032858/378 ./public/node3.js:70032858/245

--- public/node3.js
+++ public/node3.js
@@ -1,3 +1,7 @@
 document.getElementById("btnback1").onclick = function () {
   location.href = "page2.html";
 }
+
+document.getElementById("changename").onclick = function changename () {
+  document.getElementById("name").innerHTML = window.prompt("Enter desired name");
+}
