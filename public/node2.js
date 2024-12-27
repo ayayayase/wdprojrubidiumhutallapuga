a10c7333c8b66b3d8db1a0b2d170d913d59177c2
Glitch (glitch-hello-node) <none>
2024-12-27 11:14:09 +0000

    🐲🚊 Checkpoint ./public/node2.js:70032858/96 ./public/node3.js:70032858/10 ./public/page2.html:70032858/204 ./public/page3.html:70032858/171 ./public/style3.css:70032858/271 ./public/style2.css:70032858/271 ./.glitch-assets:70032858/345

--- public/node2.js
+++ public/node2.js
@@ -1,3 +1,7 @@
 document.getElementById("btnback1").onclick = function () {
   location.href = "index.html";
 }
+
+document.getElementById("btnstudio").onclick = function () {
+  location.href = "page3.html";
+}
