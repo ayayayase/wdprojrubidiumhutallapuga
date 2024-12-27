4154d4ff52db0668bdb37b86ddc88371c5c3e0f9
Glitch (glitch-hello-node) <none>
2024-12-27 11:03:06 +0000

    🏋🌎 Checkpoint ./public/style2.css:70032858/139 ./public/node2.js copy:70032858/92 ./public/node2.js:70032858/19 ./public/page2.html:70032858/50 ./public/nodehome.js:70032858/102 ./public/page3.html :70032858/4625 ./.glitch-assets:70032858/528 ./public/page3.html:70032858/1084 ./public/node3.js :70032858/93 ./public/style3.css :70032858/1217 ./public/style3.css:70032858/1722

---
+++ public/node3.js
@@ -0,0 +1,3 @@
+document.getElementById("btnback1").onclick = function () {
+  location.href = "index.html";
+}
--- public/nodehome.js
+++ public/nodehome.js
@@ -1,3 +1,7 @@
 document.getElementById("btnstrt").onclick = function () {
   location.href = "page2.html";
 }
+
+document.getElementById("btnstudio").onclick = function () {
+  location.href = "page3.html";
+}
