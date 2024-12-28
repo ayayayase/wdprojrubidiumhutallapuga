1b454cd950aa14df39a78817e2108de405d5851a
Glitch (glitch-hello-node) <none>
2024-12-28 08:06:54 +0000

    🍐🏞 Checkpoint ./public/node3.js:70032858/387 ./public/page2.html:70032858/162 ./public/page3.html:70032858/80

--- public/node3.js
+++ public/node3.js
@@ -13,13 +13,32 @@ document.getElementById("changedialogue").onclick = function changedialogue () {
   document.getElementById("dialogue").innerHTML = dialogueinput;
 }
 
+const ids = [
+  "title",
+  "caption",
+  "hrtlndecor",
+  "changename",
+  "changebackground",
+  "dialogueinput",
+  "changedialogue"
+];
+
 document.getElementById("hideshow").onclick = function hide () {
   document.getElementById("hideshow").style.display = "none";
+  
+  for (let i = 0; i < ids.length; i++) {
+  document.getElementById(ids[i]).style.display = "none";
+}
   
 document.getElementById("showhide").style.display = "inline";
 }
 
 document.getElementById("showhide").onclick = function hide () {
   document.getElementById("showhide").style.display = "none";
+  
+    for (let i = 0; i < ids.length; i++) {
+  document.getElementById(ids[i]).style.display = "inline";
+}
+  
 document.getElementById("hideshow").style.display = "inline";
 }
